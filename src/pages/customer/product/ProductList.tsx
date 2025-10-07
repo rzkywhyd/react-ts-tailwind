35e97dfa5859f19a105f2bf5c8eeb01548b0d4b9
Rizky Wahyudi <rzkywhyd97@gmail.com>
2025-10-07 17:32:11 +0700

    Add Filter Product

--- src/pages/customer/product/ProductList.tsx
+++ src/pages/customer/product/ProductList.tsx
@@ -24,7 +24,11 @@ const ProductList = () => {
   return (
     <div className="pt-30 px-6">
       <div className="grid grid-cols-12 w-full">
-        <div className="col-span-2">Filter</div>
+        <div className="col-span-2 mt-4">
+          <h3 className="font-bold">FILTER</h3>
+          <input type="text" name="minPrice" id="minPrice" className="mt-2 border-2 " placeholder="Max Price"/>          
+          <input type="text" name="maxPrice" id="maxPrice" className="mt-2 border-2 " placeholder="Min Price"/>
+        </div>
         <div className="col-span-10">
           <Card products={products} />
         </div>
